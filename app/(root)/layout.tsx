5030161668b031f60396ccead2e1ed1852db18ad
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2023-08-31 03:34:08 -0400

    Created personal spaces, Topbar (navbar) and LeftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,19 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
-
 import "../globals.css";
+import type { Metadata } from 'next';
 
-export const metadata = {
+import Topbar from '@/components/shared/Topbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Bottombar from '@/components/shared/Bottombar';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
   title: "Threads App",
   description: "A Next.js 13 Meta Threads Application"
 }
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -18,8 +23,24 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>
-          {children}
+        <body className={inter.className}>
+          <Topbar />
+
+            <main>
+
+              <LeftSidebar />
+
+              <section className="main-container">
+                <div className="w-full max-w-4xl">
+                  {children}
+                </div>
+              </section>
+
+              <RightSidebar />
+
+            </main>
+
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
